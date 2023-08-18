37e43693dc2e23a802b913c1172225081df8c6af
Rahul Gill <rgill1@protonmail.com>
2023-08-18 15:10:39 +0530

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { page } from '$app/stores';
+    import {base} from "$app/paths"
+</script>
+
+
+<div class="prose dark:prose-invert mt-10 mx-auto text-center">
+    <h1>Oops! {$page.error.message}</h1>
+    <a href="{base}/" class="no-underline"><h2 class="hover:primary-coloring">Go to Home page</h2></a>
+</div>

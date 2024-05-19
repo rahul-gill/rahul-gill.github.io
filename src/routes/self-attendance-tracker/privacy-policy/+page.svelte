816d04ac8657eae16e5e9938fd3ba3957633c7b3
Rahul Gill <rgill1@protonmail.com>
2024-05-19 17:56:32 +0530

    app privacy policy

---
+++ src/routes/self-attendance-tracker/privacy-policy/+page.svelte
@@ -0,0 +1,23 @@
+<div class="prose dark:prose-invert mt-10 mx-auto">
+    <h1 class="text-5xl">Self Attendance Tracker Privacy Policy</h1>
+
+    <p>
+        This privacy policy governs your use of the software application Self
+        Attendance Tracker for mobile devices
+    </p>
+
+    <h2>What information does the Application obtain and how is it used?</h2>
+    <p>
+        This application allows users to store attendance records. All the user
+        data remains on the device and is not transmitted off of the device by
+        this app. No information is automatically collected from the Application
+        and transmitted off of the mobile device, either automatically or
+        manually.
+    </p>
+    <h2>Contact us</h2>
+    <p>
+        If you have any questions regarding privacy while using the Application,
+        or have questions about our practices, please contact us via email at
+        rahulgill.dev@gmail.com .
+    </p>
+</div>
